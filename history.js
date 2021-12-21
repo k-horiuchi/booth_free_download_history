94cf0778fc31b17c2c3cb95bb7fd3a9c86d3cf6d
Kanta Horiuchi <47852441+k-horiuchi@users.noreply.github.com>
2021-12-21 17:34:18 +0900

    軽微な修正

--- history.js
+++ history.js
@@ -1,6 +1,6 @@
 chrome.storage.local.get("booth_history", (result) => {
     const storage = encodeURI(JSON.stringify(result.booth_history)).replace(/%../g, "*").length/1000
-    document.querySelector("#storage").innerText = `ストレージ使用量：${storage}MB / 5MB`
+    document.querySelector("#storage").innerText = `ストレージ使用量：${storage.toFixed(1)}MB / 5.0MB`
     
     for(let key in result.booth_history)
     {
@@ -10,6 +10,13 @@ chrome.storage.local.get("booth_history", (result) => {
     if(storage > 4.5)
     {
         alert("ストレージの使用量が4.5MB以上になりました。「履歴の保存」と「履歴の全削除」を行ってください。保存できなくなります")
+    }
+
+    if(storage.toFixed(1) == 0.0)
+    {
+        document.querySelector("h2").innerText = "まだ何もダウンロードしていません。"
+        document.querySelector("#download").style.display ="none"
+        document.querySelector("#delete").style.display ="none"
     }
 })
 
