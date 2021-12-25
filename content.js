7ac14888967780c5adf14c938ab7e3ca9f2f94bc
Kanta Horiuchi <47852441+k-horiuchi@users.noreply.github.com>
2021-12-25 21:01:50 +0900

    バージョン4対応

--- content.js
+++ content.js
@@ -35,7 +35,10 @@ const buttonChange = (item) => {
             result.booth_history = !result.booth_history ? {} : result.booth_history
             result.booth_history[location.href] = `[${dateStr}] ${title} / ${author}`
             chrome.storage.local.set({"booth_history": result.booth_history}, ()=>{})
+            const showBox = `<div class="u-mb-300 u-tpg-footnote u-text-gray-500">シェアすると、クリエイターの創作活動の大きな支援になります。<iframe id="twitter-widget-0" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen="true" class="twitter-share-button twitter-share-button-rendered twitter-tweet-button" style="position: static; visibility: visible; width: 89px; height: 20px;" title="Twitter Tweet Button" src="https://platform.twitter.com/widgets/tweet_button.21f942bb866c2823339b839747a0c50c.ja.html#dnt=false&amp;hashtags=booth_pm&amp;id=twitter-widget-0&amp;lang=ja&amp;original_referer=${location.href}&amp;size=m&amp;text=「${title}」 を ${author} でダウンロードしました！&amp;time=${new Date().getTime()}&amp;type=share&amp;url=${location.href}" data-url="${location.href}"></iframe></div>`
+            item.insertAdjacentHTML("afterend",showBox)
         })
         window.location.href = link
     })
 }
+
